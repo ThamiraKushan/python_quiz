c5c80323af70614c1c0f721f605c13d970581899
Thamira Kushan <kushan.maduranga@layoutindex.lk>
2023-03-24 02:10:48 +0530

    login and file upload

--- quiz_window.py
+++ quiz_window.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_quiz(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(649, 429)
@@ -129,7 +129,7 @@ if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_quiz()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
