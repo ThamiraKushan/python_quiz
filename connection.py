90376d4d28997cd1fdbcc3f99e213f09a84e8b63
Thamira Kushan <kushan.maduranga@layoutindex.lk>
2023-03-25 14:53:04 +0530

    thamira registration

--- connection.py
+++ connection.py
@@ -1,5 +1,5 @@
 import mysql.connector as conn
 
 db_connection = conn.connect(
-    host="localhost", username="root", password="root", database="quiz_app"
+    host="localhost", username="root", password="root", database='quiz_db'
 )
